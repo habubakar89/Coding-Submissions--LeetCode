67f0e80db61f66f97c78eab3e10e860b5805c836
Hamza Abubakar Kheruwala <habubakar89@gmail.com>
2022-05-26 07:56:20 -0400

    Time: 1 ms (85.14%), Space: 43.4 MB (54.86%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
@@ -15,19 +15,29 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
+        //Declare the DS
         Queue<TreeNode> queue = new LinkedList<>();
         List<List<Integer>> list = new LinkedList<List<Integer>>();
         
-        //Add root to the tree
-        queue.offer(root);
+        //Check for the edge case
         if(root == null) return list;
         
+        //Add the root to the queue
+        queue.offer(root);
+        
+        //Iterate the current level of the queue
         while(!queue.isEmpty()){
-            int num = queue.size();
-            List<Integer> temp = new LinkedList<Integer>();
-            for(int i = 0 ; i < num ; i++){
+            int queueSize = queue.size();
+            List<Integer> temp = new LinkedList<>();
+            
+            //Iterating through the children of the current queue
+            for(int i = 0 ; i < queueSize ; i++){
+                
+                //Check and add the children if they exist
                 if(queue.peek().left != null) queue.offer(queue.peek().left);
                 if(queue.peek().right != null) queue.offer(queue.peek().right);
+                
+                //Add the root value to the list
                 temp.add(queue.poll().val);
             }
             list.add(temp);
