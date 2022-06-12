64b2b94850055bc64aadfefab908ae7a05e3d4b0
Hamza Abubakar Kheruwala <habubakar89@gmail.com>
2022-06-12 18:08:29 -0400

    Added solution - LeetHub

---
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
@@ -0,0 +1,69 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class DriverClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            int V = sc.nextInt();
+            int E = sc.nextInt();
+            for (int i = 0; i < V + 1; i++)
+                list.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                int u = sc.nextInt();
+                int v = sc.nextInt();
+                list.get(u).add(v);
+            }
+            if (new Solution().isCyclic(V, list) == true)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}// } Driver Code Ends
+
+
+/*Complete the function below*/
+
+class Solution {
+    // Function to detect cycle in a directed graph.
+    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+        //Declaring the DS
+        boolean[] visited = new boolean[V];
+        boolean[] dfsVisited = new boolean[V];
+        
+        //Check for all the nodes in the graph
+        for(int i = 0 ; i < V ; i++){
+            if(visited[i] == false){
+                if(checkForCycle( i , visited , dfsVisited , adj)) return true;
+            }
+            
+        }
+        
+        return false;
+    }
+    
+    public boolean checkForCycle(int node , boolean[] visited , boolean[] dfsVisited , ArrayList<ArrayList<Integer>> adj){
+        
+        //Assign visited to the current node
+        visited[node] = true;
+        dfsVisited[node] = true;
+        
+        for(int it : adj.get(node)){
+            if(!visited[it]){
+                if(checkForCycle(it , visited , dfsVisited , adj)) return true;
+            }
+            else if(dfsVisited[it]) return true;
+        }
+        
+        
+        dfsVisited[node] = false;
+        return false;
+    }
+    
+}
