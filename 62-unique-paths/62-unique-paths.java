862ac997d695147fecc191add83813166c116fd9
Hamza Abubakar Kheruwala <habubakar89@gmail.com>
2022-09-27 09:58:01 -0400

    Time: 0 ms (100.00%), Space: 41.5 MB (17.07%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,21 +1,11 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        //DP approach
-        int[][] dp = new int[m][n];
-        for(int[] row : dp) Arrays.fill(row,-1);
-        int i = 0 , j = 0;
+        //Combinatrics problem
+        int N = m + n - 2;
+        int r = m - 1;
+        double result = 1;
+        for(int i = 1 ; i <= r ; i++) result = result * (N - r + i) / i;
         
-        return countPaths(i , j , m , n , dp);
-        
-    }
-    public int countPaths(int i , int j , int m , int n , int[][] dp){
-        //Edge case - Out of Bounds
-        if(i >= m || j >= n) return 0;
-        
-        //Edge case - Goal reached
-        if(i == m - 1 && j == n - 1) return 1;
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        return dp[i][j] = countPaths(i + 1 , j , m , n , dp) + countPaths(i , j + 1, m , n , dp);
+        return (int) result;
     }
 }
