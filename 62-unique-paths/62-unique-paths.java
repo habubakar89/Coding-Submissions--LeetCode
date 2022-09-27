752e64cfeea67dd2682a7c50e708ccf2414ba795
Hamza Abubakar Kheruwala <habubakar89@gmail.com>
2022-09-27 09:42:11 -0400

    Time: 1 ms (56.38%), Space: 41 MB (50.06%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,23 +1,21 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        //Dynamic Programming Approach
+        //DP approach
         int[][] dp = new int[m][n];
         for(int[] row : dp) Arrays.fill(row,-1);
-        
         int i = 0 , j = 0;
         
+        return countPaths(i , j , m , n , dp);
         
-        return countPaths(i , j , m ,n, dp);
     }
-    public int countPaths(int i , int j , int m , int n, int[][] dp){
-        //Edge cases - Boundary
+    public int countPaths(int i , int j , int m , int n , int[][] dp){
+        //Edge case - Out of Bounds
         if(i >= m || j >= n) return 0;
         
-        //Edge cases - Goal Reached
+        //Edge case - Goal reached
         if(i == m - 1 && j == n - 1) return 1;
         
         if(dp[i][j] != -1) return dp[i][j];
-        return dp[i][j] = countPaths(i , j + 1 , m ,n, dp) + countPaths(i + 1 , j , m ,n, dp);
-        
+        return dp[i][j] = countPaths(i + 1 , j , m , n , dp) + countPaths(i , j + 1, m , n , dp);
     }
 }
