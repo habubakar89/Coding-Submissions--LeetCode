36bdc11982496c480be20e6e945110df99938099
Hamza Abubakar Kheruwala <habubakar89@gmail.com>
2022-10-19 16:18:01 -0700

    Time: 0 ms (100.00%), Space: 44 MB (87.18%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,8 +1,9 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        
         int col0 = 1;
-        int row = matrix.length , col = matrix[0].length;
+        
+        int row = matrix.length;
+        int col = matrix[0].length;
         
         for(int i = 0 ; i < row ; i++){
             if(matrix[i][0] == 0) col0 = 0;
@@ -12,11 +13,11 @@ class Solution {
         }
         
         for(int i = row - 1 ; i >= 0 ; i--){
-            
             for(int j = col - 1 ; j >= 1 ; j--){
                 if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
             }
             if(col0 == 0) matrix[i][0] = 0;
         }
+        
     }
 }
